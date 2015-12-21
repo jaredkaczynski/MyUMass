b5a0a5435365d3cc695a15962032d57dd8795d6c
Jared <razrsword@gmail.com>
2015-12-21 17:58:30 -0500

    Making it download an XML file of map data for searching for now. It's only 6MB so yolo

--- app/src/main/java/razrsword/activities/UMassMapActivity.java
+++ app/src/main/java/razrsword/activities/UMassMapActivity.java
@@ -1,13 +1,17 @@
 package razrsword.activities;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -25,12 +29,21 @@ import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.location.NominatimPOIProvider;
 import org.osmdroid.bonuspack.location.OverpassAPIProvider;
 import org.osmdroid.bonuspack.location.POI;
+import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import razrsword.main.R;
@@ -44,6 +57,9 @@ public class UMassMapActivity extends AppCompatActivity {
      */
     private GoogleApiClient client;
     private Context context;
+    private ProgressDialog pDialog;
+    // Progress dialog type (0 - for Horizontal progress bar)
+    public static final int progress_bar_type = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,6 +109,24 @@ public class UMassMapActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(UMassMapActivity.this.getContext(), pois.get(0).mDescription, Toast.LENGTH_LONG);
                         toast.show();
                     }*/
+                    File file = new File(Environment
+                            .getExternalStorageDirectory().toString() + "map.xml");
+                    if(!file.exists()){
+                        new DownloadFileFromURL().doInBackground("104.238.215.86/map.osm");
+                        pDialog = new ProgressDialog(UMassMapActivity.this);
+                        pDialog.setMessage("Downloading Map Data...");
+                        pDialog.setIndeterminate(false);
+                        pDialog.setMax(100);
+                        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                        pDialog.setCancelable(true);
+                        pDialog.show();
+                        //Toast toast = Toast.makeText(UMassMapActivity.this, "Downloading Map Data", Toast.LENGTH_LONG);
+                        //toast.show();
+                    }
+                    /*Marker startMarker = new Marker(map);
+                    startMarker.setPosition(GeoPoint.fromIntString(edittext.getText().toString()));
+                    startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    map.getOverlays().add(startMarker);*/
 
                     Toast toast = Toast.makeText(UMassMapActivity.this, "No Result Found", Toast.LENGTH_LONG);
                     toast.show();
@@ -105,12 +139,33 @@ public class UMassMapActivity extends AppCompatActivity {
 
 
 
+
         /*Toast toast = Toast.makeText(this, OpenStreetMapTileProviderConstants.getUserAgentValue(), Toast.LENGTH_LONG);
         toast.show();*/
         //Button button =
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * Showing Dialog
+     * */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case progress_bar_type:
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Downloading file. Please wait...");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
     }
         /*
     @Override
@@ -154,7 +209,89 @@ public class UMassMapActivity extends AppCompatActivity {
     }*/
 
 
+    /**
+     * Background Async Task to download file
+     * */
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
 
+        /**
+         * Before starting background thread Show Progress Bar Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                // this will be useful so that you can show a tipical 0-100%
+                // progress bar
+                int lenghtOfFile = connection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream(Environment
+                        .getExternalStorageDirectory().toString()
+                        + "/map.xml");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            pDialog.dismiss();
+        }
+
+    }
 
     public Context getContext(){
         return context;
