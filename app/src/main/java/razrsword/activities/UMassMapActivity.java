52704f6ae2b152030e71c0021e70eeb1570b486f
Jared <razrsword@gmail.com>
2015-12-21 01:45:30 -0500

    Mapping work

---
+++ app/src/main/java/razrsword/activities/UMassMapActivity.java
@@ -0,0 +1,25 @@
+package razrsword.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+import razrsword.main.R;
+
+public class UMassMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umass_map);
+        MapView map = (MapView) findViewById(R.id.gridview);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
