c774b76868b1597c05a054f80bc84c4b244b8546
Jared <razrsword@gmail.com>
2015-12-22 01:49:49 -0500

    Map works well enough

--- app/src/main/java/razrsword/mapping/UMassMapActivity.java
+++ app/src/main/java/razrsword/mapping/UMassMapActivity.java
@@ -1,9 +1,16 @@
 package razrsword.mapping;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.TintManager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -47,13 +54,14 @@ public class UMassMapActivity extends AppCompatActivity {
     private int zoomLevel = 14;
     // Progress dialog type (0 - for Horizontal progress bar)
     public static final int progress_bar_type = 0;
+    private double[] userLocation = new double[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = this;
         setContentView(R.layout.activity_umass_map);
-        OpenStreetMapTileProviderConstants.setUserAgentValue("Razrsword's UMass App V.05");
+        OpenStreetMapTileProviderConstants.setUserAgentValue("Razrsword's UMass App V.1");
         map = (MapView) findViewById(R.id.map);
         GeoPoint viewPoint = new GeoPoint(42.38955, -72.52817);
         GeoPoint markerPoint;
@@ -64,11 +72,11 @@ public class UMassMapActivity extends AppCompatActivity {
         map.setMultiTouchControls(true);
         map.setTilesScaledToDpi(true);
 
-        if(savedInstanceState != null){
-            viewPoint = new GeoPoint(savedInstanceState.getDouble("viewLat"),savedInstanceState.getDouble("viewLon"));
+        if (savedInstanceState != null) {
+            viewPoint = new GeoPoint(savedInstanceState.getDouble("viewLat"), savedInstanceState.getDouble("viewLon"));
             zoomLevel = savedInstanceState.getInt("Zoom");
-            if(savedInstanceState.getDouble("Latitude") != 0){
-                markerPoint = new GeoPoint(savedInstanceState.getDouble("Latitude"),savedInstanceState.getDouble("Longitude"));
+            if (savedInstanceState.getDouble("Latitude") != 0) {
+                markerPoint = new GeoPoint(savedInstanceState.getDouble("Latitude"), savedInstanceState.getDouble("Longitude"));
                 startMarker = new Marker(map);
                 startMarker.setPosition(markerPoint);
                 startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
@@ -76,14 +84,14 @@ public class UMassMapActivity extends AppCompatActivity {
             }
             mapController.setCenter(viewPoint);
             mapController.setZoom(zoomLevel);
-        }else {
+        } else {
             mapController.setCenter(viewPoint);
             mapController.setZoom(zoomLevel);
         }
 
         final ImageButton button = (ImageButton) findViewById(R.id.hideKeyboard);
         final AutoCompleteTextView edittext = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
-
+        final FloatingActionButton locateMyself = (FloatingActionButton) findViewById(R.id.fab);
         final List<Place> listOfPlace = getPlaces();
 
 
@@ -94,6 +102,18 @@ public class UMassMapActivity extends AppCompatActivity {
 
                 inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
                         InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        });
+
+        locateMyself.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                locateMyself.setBackgroundColor(0x881C1C);
+                userLocation = getGPS();
+                Marker userMarker = new Marker(map);
+                userMarker.setPosition(new GeoPoint(userLocation[0], userLocation[1]));
+                userMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(userMarker);
+                map.getController().animateTo(new GeoPoint(userLocation[0], userLocation[1]));
             }
         });
 
@@ -150,10 +170,42 @@ public class UMassMapActivity extends AppCompatActivity {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
 
+                    int listPosition = 0;
+                    double topPosition = 1;
+                    for (int i = 0; i < listOfPlace.size(); i++) {
+                        if (listOfPlace.get(i).getName().compareTo(edittext.getText().toString()) == 0) {
+                            listPosition = i;
+                            break;
+                        } else {
+                           if(Math.abs(listOfPlace.get(i).getName().compareTo(edittext.getText().toString())) <= topPosition){
+                               topPosition = Math.abs(listOfPlace.get(i).getName().compareTo(edittext.getText().toString()));
+                               listPosition = i;
+                           }
+                        }
+                    }
+                    InputMethodManager inputManager = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
 
-                    Toast toast = Toast.makeText(UMassMapActivity.this, "fudge", Toast.LENGTH_LONG);
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                    GeoPoint goalLocation = new GeoPoint(Double.valueOf(listOfPlace.get(listPosition).getLatitude()), Double.valueOf(listOfPlace.get(listPosition).getLongitude()));
+
+                    //Toast toast = Toast.makeText(UMassMapActivity.this,listOfPlace.get(listPosition).getLatitude() + " ", Toast.LENGTH_LONG);
+                    //toast.show();
+                    startMarker = new Marker(map);
+                    startMarker.setPosition(goalLocation);
+                    startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    startMarker.setTitle(listOfPlace.get(listPosition).getName());
+                    //map.getController().setCenter(goalLocation);
+                    map.getOverlays().clear();
+                    map.getController().setZoom(16);
+                    map.getOverlays().add(startMarker);
+                    map.invalidate();
+                    map.getController().animateTo(goalLocation);
+
+                    /*Toast toast = Toast.makeText(UMassMapActivity.this, "fudge", Toast.LENGTH_LONG);
                     toast.show();
-                    /*Marker startMarker = new Marker(map);
+                    Marker startMarker = new Marker(map);
                     startMarker.setPosition(GeoPoint.fromIntString(edittext.getText().toString()));
                     startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
                     map.getOverlays().add(startMarker);*/
@@ -180,6 +232,35 @@ public class UMassMapActivity extends AppCompatActivity {
                 new InputStreamReader(is));
         places = parser.parse(is);
         return places;
+    }
+
+    private double[] getGPS() {
+        LocationManager lm = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE);
+        List<String> providers = lm.getProviders(true);
+
+        Location l = null;
+
+        for (int i = providers.size() - 1; i >= 0; i--) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                double[] gps = new double[2];
+                if (l != null) {
+                    gps[0] = 42.38955;
+                    gps[1] = -72.52817;
+                }
+                return gps;
+            }
+            l = lm.getLastKnownLocation(providers.get(i));
+            if (l != null) break;
+        }
+
+        double[] gps = new double[2];
+        if (l != null) {
+            gps[0] = l.getLatitude();
+            gps[1] = l.getLongitude();
+        }
+
+        return gps;
     }
 
     @Override
