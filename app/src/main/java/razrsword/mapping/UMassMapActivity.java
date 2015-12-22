e3c8960db0cb5fe777378a6c19f6dff728983656
Jared <razrsword@gmail.com>
2015-12-22 00:42:49 -0500

    Successfully saves state between rotation

--- app/src/main/java/razrsword/mapping/UMassMapActivity.java
+++ app/src/main/java/razrsword/mapping/UMassMapActivity.java
@@ -1,15 +1,9 @@
 package razrsword.mapping;
 
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.res.AssetManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -17,7 +11,6 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -29,21 +22,11 @@ import org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -59,6 +42,9 @@ public class UMassMapActivity extends AppCompatActivity {
     private GoogleApiClient client;
     private Context context;
     private ProgressDialog pDialog;
+    private Marker startMarker;
+    MapView map;
+    private int zoomLevel = 14;
     // Progress dialog type (0 - for Horizontal progress bar)
     public static final int progress_bar_type = 0;
 
@@ -68,20 +54,34 @@ public class UMassMapActivity extends AppCompatActivity {
         context = this;
         setContentView(R.layout.activity_umass_map);
         OpenStreetMapTileProviderConstants.setUserAgentValue("Razrsword's UMass App V.05");
-        final MapView map = (MapView) findViewById(R.id.map);
-        GeoPoint startPoint = new GeoPoint(42.38955, -72.52817);
+        map = (MapView) findViewById(R.id.map);
+        GeoPoint viewPoint = new GeoPoint(42.38955, -72.52817);
+        GeoPoint markerPoint;
+        IMapController mapController = map.getController();
 
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
-        IMapController mapController = map.getController();
-        mapController.setCenter(startPoint);
-        mapController.setZoom(14);
         map.setTilesScaledToDpi(true);
 
+        if(savedInstanceState != null){
+            viewPoint = new GeoPoint(savedInstanceState.getDouble("viewLat"),savedInstanceState.getDouble("viewLon"));
+            zoomLevel = savedInstanceState.getInt("Zoom");
+            if(savedInstanceState.getDouble("Latitude") != 0){
+                markerPoint = new GeoPoint(savedInstanceState.getDouble("Latitude"),savedInstanceState.getDouble("Longitude"));
+                startMarker = new Marker(map);
+                startMarker.setPosition(markerPoint);
+                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(startMarker);
+            }
+            mapController.setCenter(viewPoint);
+            mapController.setZoom(zoomLevel);
+        }else {
+            mapController.setCenter(viewPoint);
+            mapController.setZoom(zoomLevel);
+        }
 
-
-        final ImageButton button = (ImageButton) findViewById(R.id.imageButton);
+        final ImageButton button = (ImageButton) findViewById(R.id.hideKeyboard);
         final AutoCompleteTextView edittext = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
 
         final List<Place> listOfPlace = getPlaces();
@@ -127,15 +127,17 @@ public class UMassMapActivity extends AppCompatActivity {
 
                 //Toast toast = Toast.makeText(UMassMapActivity.this,listOfPlace.get(listPosition).getLatitude() + " ", Toast.LENGTH_LONG);
                 //toast.show();
-                Marker startMarker = new Marker(map);
+                startMarker = new Marker(map);
                 startMarker.setPosition(goalLocation);
                 startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
                 startMarker.setTitle(listOfPlace.get(listPosition).getName());
                 //map.getController().setCenter(goalLocation);
+                map.getOverlays().clear();
                 map.getController().setZoom(16);
                 map.getOverlays().add(startMarker);
                 map.invalidate();
                 map.getController().animateTo(goalLocation);
+
                 Log.v("Map stuff", goalLocation.getLatitude() + " " + listOfPlace.get(listPosition).getLatitude());
                 Log.v("Map stuff 2", goalLocation.getLongitude() + " " + listOfPlace.get(listPosition).getLongitude());
                 Log.v("Map stuff 3", listPosition + " ");
@@ -163,16 +165,6 @@ public class UMassMapActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-        /*Toast toast = Toast.makeText(this, OpenStreetMapTileProviderConstants.getUserAgentValue(), Toast.LENGTH_LONG);
-        toast.show();*/
-        //Button button =
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public List<Place> getPlaces() {
@@ -189,6 +181,38 @@ public class UMassMapActivity extends AppCompatActivity {
         places = parser.parse(is);
         return places;
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        if(this.startMarker != null) {
+            savedInstanceState.putDouble("Latitude", UMassMapActivity.this.startMarker.getPosition().getLatitude());
+            savedInstanceState.putDouble("Longitude", UMassMapActivity.this.startMarker.getPosition().getLongitude());
+        } else {
+            savedInstanceState.putDouble("Latitude", 0);
+            savedInstanceState.putDouble("Longitude", 0);
+        }
+        Log.v("onSaveInstanceState", " saving data between instances" + UMassMapActivity.this.map.getMapCenter().getLatitude() + " " + UMassMapActivity.this.map.getZoomLevel());
+        savedInstanceState.putDouble("viewLat", UMassMapActivity.this.map.getMapCenter().getLatitude());
+        savedInstanceState.putDouble("viewLon", UMassMapActivity.this.map.getMapCenter().getLongitude());
+        savedInstanceState.putInt("Zoom", UMassMapActivity.this.map.getZoomLevel());
+        // etc.
+    }
+    /*
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        double Latitude = savedInstanceState.getDouble("Latitude");
+        double Longitude = savedInstanceState.getDouble("Longitude");
+        double viewLat = savedInstanceState.getDouble("viewLat");
+        double viewLon = savedInstanceState.getDouble("viewLon");
+        int zoom = savedInstanceState.getInt("Zoom");
+    }*/
 
     public List<String> placesToNames(List<Place> places) {
         List<String> placenames = new ArrayList<>();
