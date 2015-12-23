0b66a727c79e4ffdec21a73ad40fabc300ed488b
Jared <razrsword@gmail.com>
2015-12-23 15:57:50 -0500

    I think I have the basics of a cardview fragment but it doesn't work... yet

--- app/src/main/java/razrsword/dining/DiningAdapter.java
+++
@@ -1,7 +0,0 @@
-package razrsword.dining;
-
-/**
- * Created by razrs on 23-Dec-15.
- */
-public class DiningAdapter {
-}
---
+++ app/src/main/java/razrsword/dining/DiningLocation.java
@@ -0,0 +1,11 @@
+package razrsword.dining;
+
+/**
+ * Created by razrs on 23-Dec-15.
+ */
+public class DiningLocation {
+    String locationName;
+    public DiningLocation(String name){
+        locationName = name;
+    }
+}
