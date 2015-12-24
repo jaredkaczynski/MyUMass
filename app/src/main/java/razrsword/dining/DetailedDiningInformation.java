799e75ecf03e24bc6438d661ac1a75c04d544d23
Jared <razrsword@gmail.com>
2015-12-24 00:17:37 -0500

    Fixed transitions to get the correct view

--- app/src/main/java/razrsword/dining/DetailedDiningInformation.java
+++ app/src/main/java/razrsword/dining/DetailedDiningInformation.java
@@ -1,12 +1,15 @@
 package razrsword.dining;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 
 import razrsword.main.R;
@@ -20,14 +23,21 @@ public class DetailedDiningInformation extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         AppCompatImageView test = (AppCompatImageView) this.findViewById(R.id.dining_location_image);
+        int statusBarHeight = (int) Math.ceil(25 * DetailedDiningInformation.this.getResources().getDisplayMetrics().density);
+        test.setPadding(0,getStatusBarHeight(),0,0);
         test.setScaleType(ImageView.ScaleType.CENTER_CROP);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setVisibility(View.INVISIBLE);
     }
+    int getStatusBarHeight(){
+        Rect rectangle = new Rect();
+        Window window = getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+        int statusBarHeight = rectangle.top;
+        int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int titleBarHeight= contentViewTop - statusBarHeight;
+        Log.v("statusbar height", statusBarHeight + " ");
+        return statusBarHeight;
+    }
+
 }
